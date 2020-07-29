6abbbd6c142f8ca1bbc3a80b5441fcb6e1c69209
Francisco Amorim <francisco.s.amorim@gmail.com>
2020-07-29 15:13:19 +0100

    Fix Build

--- lib/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { JobTime, JobAction } from './types';
-export declare class Tiink {
-    private jobs;
-    addJob(name: string, time: JobTime, method: Function): void;
-    setJobAction(name: string, time: JobTime, method: Function): JobAction;
-    stopJob(name: string): void;
-    restartJob(name: string): void;
-    deleteJob(name: string): void;
-}
--- lib/types/utils/utils.d.ts
+++
@@ -1,4 +0,0 @@
-import { JobTime } from '../types';
-declare const getTime: (time: JobTime) => number;
-declare const getTimeRelativeTo: (time: JobTime, target?: Date) => number;
-export { getTime, getTimeRelativeTo, };
